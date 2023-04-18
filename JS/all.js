5d908b29517fe210297486602f0ad7fe91948b0d
titanM071 <martiniclement06@gmail.com>
2023-04-18 17:02:06 +0200

    ajout d'un pack

--- JS/all.js
+++ JS/all.js
@@ -2,6 +2,6 @@
     <ul class="outset ul">
         <li class="li"><a href="../index.html">Mon Blog</a></li>
         <li id="info" class="li"><a href="info.html">à-Propos</a></li>
-        <li class="li"><a href="portefolios.html">Porte-folios</a></li>
+        <li class="li"><a href="../page/portefolios.html">Porte-folios</a></li>
     </ul>
 )
