586d5491b90e2712a0a3c769b9de6973dca18aa2
minonix <martiniclement06@gmail.com>
2024-04-20 17:54:54 +0200

    tris dans les fichier

--- JS/all.js
+++
@@ -1,7 +0,0 @@
- menu = print(
-    <ul class="outset ul">
-        <li class="li"><a href="../index.html">Mon Blog</a></li>
-        <li id="info" class="li"><a href="info.html">à-Propos</a></li>
-        <li class="li"><a href="../page/portefolios.html">Porte-folios</a></li>
-    </ul>
-)
